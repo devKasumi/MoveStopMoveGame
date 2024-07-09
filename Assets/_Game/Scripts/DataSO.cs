437ecc7639752207196ea7637ab93b54dac0bf1b
kasumi <dangnhabui@gmail.com>
2024-07-09 07:47:07 +0700

    add new mat

--- Assets/_Game/Scripts/DataSO.cs
+++ Assets/_Game/Scripts/DataSO.cs
@@ -2,17 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DataSO : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+[CreateAssetMenu(menuName = "Data/Skin Data", fileName = "Skin Data")]
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+public class DataSO : ScriptableObject
+{
+    
 }
